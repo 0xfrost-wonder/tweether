944bee9f3483aa111844b7b407be34cb1a315b11
COOLICEBLAST <cooliceblast0904@gmail.com>
2024-02-02 10:56:37 +0100

    test

---
+++ contracts/users/UserStorage.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract UserStorage {
+
+    mapping (uint => Profile) profiles;
+
+    
+    struct Profile {
+        uint id;
+        bytes32 username;
+    }
+
+
+    uint lastUserId = 0;
+
+    function createUser (bytes32 _username) public returns (uint){
+        lastUserId++;
+
+        profiles[lastUserId] = Profile(lastUserId, _username);
+        
+        return lastUserId;
+    }
+}
---
+++ test/integration/users.js
@@ -0,0 +1,13 @@
+const UserStorage = artifacts.require("UserStorage");
+
+contract('users', () => {
+
+    it("can create user", async () => {
+        const storage = await UserStorage.deployed();
+
+        const username = web3.utils.fromAscii("Tristan");
+        const tx = await storage.createUser(username);
+
+        assert.isOk(tx);
+    })
+})
